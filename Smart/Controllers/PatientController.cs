50944fadc425ff44b97b66202ef0fa1d3dea3a5e
tangyuhang0829 <m15731495370@163.com>
2021-04-21 20:25:30 +0800

    汤宇航4.21 20:25 改

---
+++ Smart/Controllers/PatientController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using SmartMedical.BLL;
+using SmartMedical.DAL;
+using SmartMedical.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using static SmartMedical.BLL.SmartMedicalBLL;
+
+namespace SmartMedical.Controllers
+{
+    [ApiController]
+    [Route("SmartMedical/Patient")]
+    public class PatientController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        SmartMedicalBLL _bll;
+        LoginTel _logintel;
+        public PatientController(SmartMedicalBLL bll, LoginTel logintel)
+        {
+            _bll = bll;
+            _logintel = logintel;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [Route("login"),HttpPost]
+        public IActionResult Login(string phone,string password)
+        {
+            int h = _bll.Login(phone,password);
+            return Ok(new { msg =h>0?"登录成功!":"账号或密码错误!",state=h > 0 ?true:false });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns></returns>
+        [Route("zhuce"),HttpPost]
+        public IActionResult Zhuce(string phone) 
+        {
+            int h = 0;
+            
+            DataSet ds = _bll.ZhuCe(phone);
+            if (ds.Tables[0].Rows.Count>0)
+            {
+                h = 0;
+            }
+            else
+            {
+                h = _bll.ZhuceIn(phone);
+            }
+            return Ok(new { msg=h>0?"注册成功！":"手机号已存在!",state=h>0?true:false});
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns></returns>
+        [Route("phoneyzm"),HttpPost]
+        public IActionResult PhoneYZM(string phone) 
+        {
+            Random r = new Random();
+            string code = r.Next(1000,9999).ToString();
+            var str = _logintel.sendSmsCode(phone,code);
+            return Ok(new { data=str,code=code});
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Route("getpatients"),HttpGet]
+        public IActionResult GetPatients() 
+        {
+            List<Patient> list = _bll.GetPatients();
+            return Ok(list);
+        }
+
+        [Route("tt"), HttpGet]
+        public IActionResult TT()
+        {
+            return Ok("1232323");
+        }
+    }
+}
---
+++ Smart/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using SmartMedical.BLL;
+using SmartMedical.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static SmartMedical.BLL.SmartMedicalBLL;
+
+namespace Smart
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Smart", Version = "v1" });
+            });
+            services.AddTransient<SmartMedicalBLL>();
+            services.AddTransient<DBHelper>();
+            services.AddTransient<LoginTel>();
+            // 设置允许所有来源跨域
+            services.AddCors(options => options.AddPolicy("1808api",
+            builder =>
+            {
+                builder.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .SetIsOriginAllowed(_ => true) // =AllowAnyOrigin()
+                    .AllowCredentials();
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Smart v1"));
+            }
+            app.UseCors("1808api");
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
